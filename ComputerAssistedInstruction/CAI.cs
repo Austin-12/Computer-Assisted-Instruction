7a9bda3d4610a0fc8e968b5f340ad8202b6114ec
Austin Joe McGill <austin-12_@hotmail.com>
2024-02-23 11:22:14 -0500

    commit 1
    2-23-2024

---
+++ ComputerAssistedInstruction/CAI.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class CAI
+{
+    private enum Status { Correct, Wrong }; //enum to represent if the quesitons are correct or not./
+    static void Main(String[] args)
+    {
+
+        //ask the question continuesly
+        Console.WriteLine("Welcome to the multiplication learning program");
+        while(true)
+        {
+            askQuestion();
+        }
+}
+
+    //method used to generate each new question. called once the app begins execution and each time the user answers the question correctly
+    static int randomNum()
+    {
+        Random random = new Random(); //random generate
+        int num = random.Next(1, 10);
+        return num;
+    }
+
+    //method to ask the math question
+    static void askQuestion()
+    {
+        int num1 = randomNum(); //1st operand
+        int num2 = randomNum(); //2nd operand
+        int answer = num1 * num2; //the correct answer
+        Status question = Status.Wrong; //question can be wrong or correct
+
+        while (question == Status.Wrong) //While the question is wrong
+        {
+            Console.WriteLine($"How much is {num1} x {num2}?");
+            int guess = int.Parse(Console.ReadLine()); //get answer from the user
+
+            if (answer == guess) //if the answer is correct 
+            {
+                Console.WriteLine("very good!");
+                question = Status.Correct;
+            }
+            else if (answer != guess) //if the answer is wrong
+            {
+                Console.WriteLine("No. please try again");
+                question = Status.Wrong;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Computer Assisted Instruction
-  This program is created to help a elementary-school student learn multiplication.
+
+This program is created to help a elementary-school student learn multiplication.
+
+**_Computer assisted instruction_**
+
+This is a program I completed after reading chapter 7 (a deeper look).
+This program is created to help a elementary-school student learn multiplication.
+After the student answers a question right they will be prompted with a new question
+continuesly, if the student gets the answer wrong they will have an infinite amount
+of chances to input the correct answer.
+
+the biggest take aways from this chapter that I didn't already know was a
+deeper look into static methods and static variables and what that word actually
+means how static methods are methods only opperate on its arguments and not dependent
+on an object I never thought about that. this chapter covered random number generator
+and enums i've read about enums once but i've never used them myself.
+
+How to use:
+Upon running the program you will be prompted with a multiplication question
+type in your answer if it is correct very good will be printed along with a new question.
